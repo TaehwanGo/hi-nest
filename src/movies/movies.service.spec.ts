dfb4b0747c4f18ba5fc15c244f6fb7db1930bee9
TaehwanGo <gth1123@naver.com>
2021-01-01 23:32:21 +0900

    #3.2 Testing getAll and getOne

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,3 +1,4 @@
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { MoviesService } from './movies.service';
 
@@ -16,7 +17,32 @@ describe('MoviesService', () => {
     expect(service).toBeDefined();
   });
 
-  it('should be 4', () => {
-    expect(2 + 3).toEqual(5);
+  describe('getAll', () => {
+    it('should return an array', () => {
+      const result = service.getAll();
+      expect(result).toBeInstanceOf(Array);
+    });
+  });
+
+  describe('getOne', () => {
+    it('should return a movie', () => {
+      // getOne은 create된 것이 없으면 문제가 생길 수 있으므로 하나 생성
+      service.create({
+        title: 'Test Movie',
+        genres: ['test'],
+        year: 2020,
+      });
+      const movie = service.getOne(1);
+      expect(movie).toBeDefined();
+      // expect(movie.id).toEqual(1);
+    });
+    it('should throw 404 error', () => {
+      try {
+        service.getOne(999); // id에 맞는 데이터가 없으면 throw new NotFoundException 에 의해 에러를 던짐
+      } catch (error) {
+        expect(error).toBeInstanceOf(NotFoundException);
+        // expect(error.message).toEqual('Movie with ID: 999 not found.');
+      }
+    });
   });
 });
