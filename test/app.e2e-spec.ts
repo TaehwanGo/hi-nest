c32422a09d4f13a04b89a43f9d752989f76a220b
TaehwanGo <gth1123@naver.com>
2021-01-02 00:43:29 +0900

    #4.0 E2E Testing, Testing movies

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -16,9 +16,37 @@ describe('AppController (e2e)', () => {
   });
 
   it('/ (GET)', () => {
-    return request(app.getHttpServer())
+    // GET방식으로
+    return request(app.getHttpServer()) // API request를 보냄
       .get('/')
       .expect(200)
-      .expect('Hello World!');
+      .expect('Welcome to my Movie API');
+  });
+
+  describe('/movies', () => {
+    it('GET', () => {
+      return request(app.getHttpServer()) // app.getHttpServer()는 우리 서버 url(http://localhost:3000) 을 의미함
+        .get('/movies')
+        .expect(200)
+        .expect([]);
+      // 보통 개발자는 2개의 DB를 사용함 : 배포용, 개발 테스트용
+      // 테스팅 DB는 데이터를 생성, 삭제하는 일이 빈번하고 처음엔 [] 임(empty)
+    });
+
+    it('POST', () => {
+      return request(app.getHttpServer()) //
+        .post('/movies')
+        .send({
+          title: 'Test',
+          year: 2020,
+          genres: ['test'],
+        })
+        .expect(201);
+    });
+    it('DELETE', () => {
+      return request(app.getHttpServer()) //404가 나오는 것 까지 테스트하는 것을 보여주고 싶었음
+        .delete('/movies')
+        .expect(404);
+    });
   });
 });
