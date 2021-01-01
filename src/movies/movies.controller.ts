d380ea2e08c153755170e670d933347dc63924b4
TaehwanGo <gth1123@naver.com>
2021-01-01 11:28:40 +0900

    #2.1 More Routers using decorators : @Param, @Body, @Query

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('movies')
 export class MoviesController {
@@ -7,15 +16,22 @@ export class MoviesController {
     return 'This will return all movies';
   }
 
-  @Get('/:id')
+  @Get('search')
+  search(@Query('year') searchingYear: string) {
+    return `we are searching for a movie made after: ${searchingYear}`;
+  }
+
+  @Get('/:id') // '/:id'로 작성해도 되고 ':id'로 작성해도 됨
   getOne(@Param('id') movieId: string) {
     // movieId는 param을 가져오지만 이름 변경 가능
     return `This will return one movie with the id: ${movieId}`;
   }
 
   @Post()
-  create() {
-    return 'This will create a movie';
+  create(@Body() movieData) {
+    // req.body 의 데이터를 가져오려면
+    console.log(movieData);
+    return movieData;
   }
 
   @Delete('/:id')
@@ -23,8 +39,12 @@ export class MoviesController {
     return `This will delete a movie with the id: ${movieId}`;
   }
 
-  @Patch('/:id')
-  patch(@Param('id') movieId: string) {
-    return `This will update a movie with the id: ${movieId}`;
+  @Patch(':id')
+  patch(@Param('id') movieId: string, @Body() updateData) {
+    return {
+      // json을 받아서 json으로 return
+      updateMovie: movieId,
+      ...updateData,
+    };
   }
 }
